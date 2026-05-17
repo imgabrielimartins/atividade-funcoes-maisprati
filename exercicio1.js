36636328a90036b4079805b5fc22dc8c1fffef85
Gabrieli <gabrielidelimamartins@gmail.com>
2026-05-16 21:14:36 -0300

    primeiro exercicio

---
+++ exercicio1.js
@@ -0,0 +1,14 @@
+/*
+Função com parâmetros e retorno: Criar uma função que calcule a média de três notas
+passadas como parâmetros.
+Instruções:
+Criar uma função que receba três parâmetros (nota1, nota2,nota3);
+A função deve calcular a média das três notas e retornar o valor;
+Chamar a função e exibir a média no console.
+*/
+
+function mediaNotas(nota1, nota2, nota3) {
+  return (nota1 + nota2 + nota3) / 3
+}
+
+console.log(mediaNotas(0,6,0))
